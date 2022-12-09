ecda84ad251e2eb32ea15fdc8c9092ad30bb23cd
Chris3606 <cridley106@gmail.com>
2022-12-08 19:40:28 -0600

    Finished day 08.

---
+++ src/bin/08.rs
@@ -0,0 +1,138 @@
+use advent_of_code::helpers::{self, Vector2i};
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let treemap = helpers::grid_from_digit_grid(input);
+
+    let mut visibility_grid = helpers::Grid::new_empty(treemap.width(), treemap.height(), false);
+
+    // Find visible cells from the north and south ends.
+    for x in 0..treemap.width() {
+        let start_pos = helpers::Vector2i { x: x as i64, y: 0 };
+        let mut max_val = *treemap.get(&start_pos);
+        *visibility_grid.get_mut(&start_pos) = true;
+        for y in 1..treemap.height() {
+            let pos = helpers::Vector2i {
+                x: x as i64,
+                y: y as i64,
+            };
+
+            let val = *treemap.get(&pos);
+            if val > max_val {
+                max_val = val;
+                *visibility_grid.get_mut(&pos) = true;
+            }
+        }
+
+        let start_pos = helpers::Vector2i {
+            x: x as i64,
+            y: treemap.height() as i64 - 1,
+        };
+        let mut max_val = *treemap.get(&start_pos);
+        *visibility_grid.get_mut(&start_pos) = true;
+        for y in (0..treemap.height()).rev() {
+            let pos = helpers::Vector2i {
+                x: x as i64,
+                y: y as i64,
+            };
+
+            let val = *treemap.get(&pos);
+            if val > max_val {
+                max_val = val;
+                *visibility_grid.get_mut(&pos) = true;
+            }
+        }
+    }
+
+    // Find visible cells from the west and east ends
+    for y in 0..treemap.height() {
+        let start_pos = helpers::Vector2i { x: 0, y: y as i64 };
+        let mut max_val = *treemap.get(&start_pos);
+        *visibility_grid.get_mut(&start_pos) = true;
+        for x in 1..treemap.width() {
+            let pos = helpers::Vector2i {
+                x: x as i64,
+                y: y as i64,
+            };
+
+            let val = *treemap.get(&pos);
+            if val > max_val {
+                max_val = val;
+                *visibility_grid.get_mut(&pos) = true;
+            }
+        }
+
+        let start_pos = helpers::Vector2i {
+            x: treemap.width() as i64 - 1,
+            y: y as i64,
+        };
+        let mut max_val = *treemap.get(&start_pos);
+        *visibility_grid.get_mut(&start_pos) = true;
+        for x in (0..treemap.width()).rev() {
+            let pos = helpers::Vector2i {
+                x: x as i64,
+                y: y as i64,
+            };
+
+            let val = *treemap.get(&pos);
+            if val > max_val {
+                max_val = val;
+                *visibility_grid.get_mut(&pos) = true;
+            }
+        }
+    }
+
+    let visible_trees = visibility_grid.iter().filter(|&l| *l).count();
+
+    Some(visible_trees)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let treemap = helpers::grid_from_digit_grid(input);
+
+    // This is somewhat brute force, but works for our input sizes.
+    let mut best_scenic_score = 0;
+    for i in 0..treemap.num_cells() {
+        let pos = Vector2i::new_from_index(i as u64, treemap.width() as u64);
+        let val = *treemap.get(&pos);
+
+        let mut scenic_score = 1;
+        for dir in helpers::CARDINAL_DIRS {
+            let mut cur = pos + &dir;
+            let mut view_dist = 0;
+            while treemap.contains(&cur) {
+                view_dist += 1;
+                if *treemap.get(&cur) >= val {
+                    break;
+                }
+                cur = cur + &dir;
+            }
+            scenic_score *= view_dist;
+        }
+        best_scenic_score = best_scenic_score.max(scenic_score);
+    }
+
+    Some(best_scenic_score)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 8);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 8);
+        assert_eq!(part_one(&input), Some(21));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 8);
+        assert_eq!(part_two(&input), Some(8));
+    }
+}
--- src/helpers.rs
+++ src/helpers.rs
@@ -3,7 +3,204 @@
  * Example import from this file: `use advent_of_code::helpers::example_fn;`.
  */
 
+use std::{ops, str::FromStr};
+
 #[derive(Debug)]
 pub enum ParseError {
     InvalidInput,
+}
+
+pub static CARDINAL_DIRS: [Vector2i; 4] = [
+    Vector2i { x: 0, y: -1 },
+    Vector2i { x: 1, y: 0 },
+    Vector2i { x: 0, y: 1 },
+    Vector2i { x: -1, y: 0 },
+];
+
+pub static EIGHTWAY_DIRS: [Vector2i; 8] = [
+    Vector2i { x: 0, y: -1 },
+    Vector2i { x: 1, y: -1 },
+    Vector2i { x: 1, y: 0 },
+    Vector2i { x: 1, y: 1 },
+    Vector2i { x: 0, y: 1 },
+    Vector2i { x: -1, y: 1 },
+    Vector2i { x: -1, y: 0 },
+    Vector2i { x: -1, y: -1 },
+];
+
+pub enum AdjacencyRule {
+    Cardinals,
+    EightWay,
+}
+
+/// Represents a 2d Vector2i with integer coordinates.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Vector2i {
+    /// The x-value of the coordinate.  Increases to the right visually.
+    pub x: i64,
+    /// The y-value of the coordinate.  Increases to the bottom visually.
+    pub y: i64,
+}
+
+impl FromStr for Vector2i {
+    type Err = ParseError;
+
+    /// Reads in a Vector2i from a string in format "x,y"
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut it = s.split(",");
+        let x = it
+            .next()
+            .ok_or(ParseError::InvalidInput)?
+            .parse::<i64>()
+            .map_err(|_| ParseError::InvalidInput)?;
+        let y = it
+            .next()
+            .ok_or(ParseError::InvalidInput)?
+            .parse::<i64>()
+            .map_err(|_| ParseError::InvalidInput)?;
+
+        Ok(Vector2i { x, y })
+    }
+}
+
+impl Vector2i {
+    /// Creates a new Vector2i.
+    pub fn new(x: i64, y: i64) -> Self {
+        Vector2i { x, y }
+    }
+
+    /// Creates a new Vector2i which represents the index value given, assuming it is encoded with
+    /// the given width value.
+    ///
+    /// Index is defined as y * width + x.
+    pub fn new_from_index(index: u64, width: u64) -> Self {
+        Vector2i {
+            x: (index % width) as i64,
+            y: (index / width) as i64,
+        }
+    }
+
+    /// Gets the "index" value associated with the given position.
+    ///
+    /// Indexes are calculated as y * width + x.  This is useful for storing a grid of Vector2is in a
+    /// 1-dimensional array.
+    pub fn to_index(&self, width: usize) -> usize {
+        (self.y as usize) * width + (self.x as usize)
+    }
+}
+
+impl ops::Add<&Vector2i> for Vector2i {
+    type Output = Self;
+
+    /// Adds the corresponding x and y components of the Vector2i together.
+    fn add(self, rhs: &Vector2i) -> Self {
+        Vector2i {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+/// Represents a 2-dimensional, integral grid of values.
+///
+/// It stores the backing values in a vector of the appropriate size, and allows you to access
+/// and/or iterate over them both by position and index.
+#[derive(Debug)]
+pub struct Grid<T> {
+    /// The values in the grid.
+    values: Vec<T>,
+    /// The width of the grid.
+    width: usize,
+}
+
+impl<T> Grid<T> {
+    /// Creates a new grid based on the values in the given vector, and the width specified.
+    ///
+    /// The values vector should have indices which corresponds to positions as defined in the
+    /// `Vector2i::to_index` function.  The vector must have exactly width * height values (and in fact
+    /// the height of the grid is inferred from the grid size and the width specified).
+    pub fn new(values: Vec<T>, width: usize) -> Self {
+        assert!(values.len() % width == 0);
+        Self { values, width }
+    }
+
+    /// Gets a reference to the value at the given position.
+    pub fn get(&self, pos: &Vector2i) -> &T {
+        &self.values[pos.to_index(self.width)]
+    }
+
+    /// Gets a mutable reference to the value at the given position.
+    pub fn get_mut(&mut self, pos: &Vector2i) -> &mut T {
+        &mut self.values[pos.to_index(self.width)]
+    }
+
+    /// Gets a reference to the value at the given index.
+    ///
+    /// The index is assumed to have been encoded by `Vector2i::to_index` (or identical math),
+    /// with a width equal to the width of the grid.
+    pub fn get_index(&self, index: usize) -> &T {
+        &self.values[index]
+    }
+
+    pub fn get_index_mut(&mut self, index: usize) -> &mut T {
+        &mut self.values[index]
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn height(&self) -> usize {
+        self.values.len() / self.width
+    }
+
+    pub fn num_cells(&self) -> usize {
+        self.values.len()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &T> {
+        self.values.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
+        self.values.iter_mut()
+    }
+
+    pub fn into_iter(self) -> impl Iterator<Item = T> {
+        self.values.into_iter()
+    }
+
+    pub fn contains(&self, pos: &Vector2i) -> bool {
+        pos.x >= 0
+            && pos.y >= 0
+            && (pos.x as usize) < self.width()
+            && (pos.y as usize) < self.height()
+    }
+}
+
+impl<T> Grid<T>
+where
+    T: std::clone::Clone,
+{
+    pub fn new_empty(width: usize, height: usize, default_value: T) -> Self {
+        Self {
+            values: vec![default_value; width * height],
+            width,
+        }
+    }
+}
+
+/// Parses a Grid<u32> from a string of the following format:
+/// ```
+/// 30373
+/// 25512
+/// 65332
+/// 33549
+/// 35390
+/// ```
+/// The grid can contain only the digits 0-9.
+pub fn grid_from_digit_grid(input: &str) -> Grid<u32> {
+    let values: Vec<_> = input.chars().filter_map(|c| c.to_digit(10)).collect();
+    let width = input.find('\n').unwrap();
+    Grid::new(values, width)
 }
