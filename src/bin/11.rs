403556cd991edbf723d48353000baebbb0cd6dba
Chris3606 <cridley106@gmail.com>
2022-12-11 10:46:23 -0600

    Cleaned up day 11 code.

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use advent_of_code::helpers::ParseError;
+use advent_of_code::helpers::{lcm, ParseError};
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 enum Operation {
@@ -83,7 +83,11 @@ impl FromStr for Monkey {
     }
 }
 
-fn simulate_monkey_business(monkeys: &mut Vec<Monkey>, rounds: u32) {
+fn simulate_monkey_business(
+    monkeys: &mut Vec<Monkey>,
+    rounds: u32,
+    worry_control: impl Fn(u64) -> u64,
+) {
     for _ in 0..rounds {
         for monkey_idx in 0..monkeys.len() {
             assert!(monkeys[monkey_idx].id == monkey_idx);
@@ -102,8 +106,8 @@ fn simulate_monkey_business(monkeys: &mut Vec<Monkey>, rounds: u32) {
                     Operation::MultOld => item * item,
                 };
 
-                // Bored with item; divide by 3
-                item /= 3;
+                // Apply worry control
+                item = worry_control(item);
 
                 // Figure out who we throw the item to
                 let throw_to = if item % monkeys[monkey_idx].test_divisor == 0 {
@@ -117,75 +121,6 @@ fn simulate_monkey_business(monkeys: &mut Vec<Monkey>, rounds: u32) {
             }
             monkeys[monkey_idx].current_items.clear();
         }
-    }
-}
-
-fn simulate_monkey_business_infinite_worry(monkeys: &mut Vec<Monkey>, rounds: u32) {
-    // Calculate the least common multiple of all the test divisors so we can avoid infinite growth.
-    let test_divisors: Vec<u64> = monkeys.iter().map(|m| m.test_divisor).collect();
-    let mut l = test_divisors[0];
-    for &num in test_divisors.iter().skip(1) {
-        l = lcm(l, num);
-    }
-
-    for _ in 0..rounds {
-        for monkey_idx in 0..monkeys.len() {
-            assert!(monkeys[monkey_idx].id == monkey_idx);
-
-            for item_idx in 0..monkeys[monkey_idx].current_items.len() {
-                // Record that this monkey inspected an item
-                monkeys[monkey_idx].items_inspected += 1;
-
-                let mut item = monkeys[monkey_idx].current_items[item_idx];
-
-                // Incrase worry level
-                item = match monkeys[monkey_idx].operation {
-                    Operation::AddConst(c) => item + c,
-                    Operation::AddOld => item + item,
-                    Operation::MultConst(c) => item * c,
-                    Operation::MultOld => item * item,
-                };
-
-                // We only use the worry level to divide by the test divisors; so mathematically
-                // it's safe to cut it down to the lcm of all the test_divisors
-                item %= l;
-
-                // Figure out who we throw the item to
-                let throw_to = if item % monkeys[monkey_idx].test_divisor == 0 {
-                    monkeys[monkey_idx].true_dest
-                } else {
-                    monkeys[monkey_idx].false_dest
-                };
-
-                // Throw to the proper monkey
-                monkeys[throw_to].current_items.push(item);
-            }
-            monkeys[monkey_idx].current_items.clear();
-        }
-    }
-}
-
-fn lcm(first: u64, second: u64) -> u64 {
-    first * second / gcd(first, second)
-}
-
-fn gcd(first: u64, second: u64) -> u64 {
-    let mut max = first;
-    let mut min = second;
-    if min > max {
-        let val = max;
-        max = min;
-        min = val;
-    }
-
-    loop {
-        let res = max % min;
-        if res == 0 {
-            return min;
-        }
-
-        max = min;
-        min = res;
     }
 }
 
@@ -195,7 +130,7 @@ pub fn part_one(input: &str) -> Option<u64> {
         .map(|m_data| m_data.parse().unwrap())
         .collect();
 
-    simulate_monkey_business(&mut monkeys, 20);
+    simulate_monkey_business(&mut monkeys, 20, |w| w / 3);
 
     let mut items_inspected: Vec<_> = monkeys.iter().map(|m| m.items_inspected).collect();
     items_inspected.sort();
@@ -208,7 +143,15 @@ pub fn part_two(input: &str) -> Option<u64> {
         .map(|m_data| m_data.parse().unwrap())
         .collect();
 
-    simulate_monkey_business_infinite_worry(&mut monkeys, 10000);
+    // Calculate the least common multiple of all the test divisors so we can avoid infinite growth.
+    // This is safe because all the algorithm does with the worry value is divide by the test_divisor,
+    // and look at the remainder, so wrapping at the lcm of all the values won't hurt.
+    let least_common_multiple = monkeys
+        .iter()
+        .map(|m| m.test_divisor)
+        .fold(1, |accum, elem| lcm(accum, elem));
+
+    simulate_monkey_business(&mut monkeys, 10000, |w| w % least_common_multiple);
 
     let mut items_inspected: Vec<_> = monkeys.iter().map(|m| m.items_inspected).collect();
     items_inspected.sort();
--- src/helpers.rs
+++ src/helpers.rs
@@ -243,3 +243,29 @@ pub fn grid_from_digit_grid(input: &str) -> Grid<u32> {
     let width = input.find('\n').unwrap();
     Grid::new(values, width)
 }
+
+/// Calculate the least common multiple of the given numbers.
+pub fn lcm(first: u64, second: u64) -> u64 {
+    first * second / gcd(first, second)
+}
+
+/// Calculate the greatest common divisor of the given numbers.
+pub fn gcd(first: u64, second: u64) -> u64 {
+    let mut max = first;
+    let mut min = second;
+    if min > max {
+        let val = max;
+        max = min;
+        min = val;
+    }
+
+    loop {
+        let res = max % min;
+        if res == 0 {
+            return min;
+        }
+
+        max = min;
+        min = res;
+    }
+}
